f76d4b9c50366ab2ec6e0bf25821a49733987095
Isabel <iarcones@gmail.com>
2019-07-11 17:59:53 -0700

    added mouseenter / mouseleave on the images

--- javascript/app.js
+++ javascript/app.js
@@ -1,9 +1,10 @@
+
 // Variables
 var limitImg = 16;
 
 // displayImages function re-renders the HTML to display the appropriate content
 
-function getTopic() {
+function getTopic () {
 
   console.log(this);
 
@@ -11,12 +12,12 @@ function getTopic() {
 
   console.log("topic-1: " + topic);
   displayImages(topic);
-  
+
 }
 
-function displayImages(topic) {
+function displayImages (topic) {
 
-  var queryURL = `https://api.giphy.com/v1/gifs/search?q=${topic}&api_key=U2E0KeRa320EgNxloxFE3VwHL3OAKsxA&limit=${limitImg}`;
+  var queryURL = `https://api.giphy.com/v1/gifs/search?q=${ topic }&api_key=U2E0KeRa320EgNxloxFE3VwHL3OAKsxA&limit=${ limitImg }`;
   console.log("queryURL: " + queryURL);
 
   // Creating an AJAX call for the specific topic button being clicked
@@ -33,13 +34,13 @@ function displayImages(topic) {
     for (var index = 0; index < limitImg; index++) {
 
       // Retrieving image info
-      var rating = response.data[index].rating;
-      var title = response.data[index].title;
+      var rating = response.data[ index ].rating;
+      var title = response.data[ index ].title;
       // var datetime = moment(response.data[index].import_datetime).format("MM/DD/YY");
-      var datetime = moment(response.data[index].import_datetime).format("MM/DD/YYYY");
+      var datetime = moment(response.data[ index ].import_datetime).format("MM/DD/YYYY");
 
-      var imgjpgURLStill = response.data[index].images.downsized_still.url;
-      var imgjpgURLAnimate = response.data[index].images.downsized.url;
+      var imgjpgURLStill = response.data[ index ].images.downsized_still.url;
+      var imgjpgURLAnimate = response.data[ index ].images.downsized.url;
 
       // // Creating html elements for info
       // var pOne = $("<p>").text("Title: " + title);
@@ -63,7 +64,7 @@ function displayImages(topic) {
       // // Adding new div to the images-view col
       // $("#images-view").append(topicDiv);
 
-      var imageCard = `<div class="col"><div class="card m-2" style="width: 16rem; height:18rem"><img src="${imgjpgURLStill}" altURL="${imgjpgURLAnimate}" class="card-img-top image-click" alt="${title}" style="width: 16rem; height:14rem"><div class="card-body p-2"><h6 class="card-title">${title}</h6><p class="card-text"><b>Rating:</b> ${rating} <br><b>Import date: </b>${datetime}</p></div>`
+      var imageCard = `<div class="col"><div class="card m-2" style="width: 16rem; height:18rem"><img src="${ imgjpgURLStill }" altURL="${ imgjpgURLAnimate }" class="card-img-top image-click" alt="${ title }" style="width: 16rem; height:14rem"><div class="card-body p-2"><h7 class="card-title">${ title }</h7><p class="card-text"><b>Rating:</b> ${ rating } <br><b>Import date: </b>${ datetime }</p></div>`
       $("#images-view").append(imageCard);
     }
   });
@@ -71,7 +72,7 @@ function displayImages(topic) {
 
 
 
-function renderButtons() {
+function renderButtons () {
 
   // Deleting the topics prior to adding new topics
   // (this is necessary otherwise you will have repeat buttons)
@@ -86,78 +87,100 @@ function renderButtons() {
     // Adding a class of topic-btn to our button
     newButton.addClass("topic-btn");
     // Adding a data-attribute
-    newButton.attr("data-name", topics[i]);
+    newButton.attr("data-name", topics[ i ]);
     // Providing the initial button text
-    newButton.text(topics[i]);
+    newButton.text(topics[ i ]);
     // Adding the button to the buttons-view div
     $("#buttons-view").append(newButton);
   }
+
 }
 
-  // This function switch images still | animated
-  function switchImages() {
+// This function switch images still | animated
+function switchImages () {
+  console.log("switchImages")
+  console.log(this)
 
-   var imageCurrent = $(this).attr("src");
-  
-    $(this).attr("src", $(this).attr("altURL"));
-    $(this).attr("altURL", imageCurrent);
+  var imageCurrent = $(this).attr("src");
 
+  $(this).attr("src", $(this).attr("altURL"));
+  $(this).attr("altURL", imageCurrent);
+
+}
+
+
+
+// This function handles events where a topic button is clicked
+$("#add-topics").on("click", function (event) {
+
+  event.preventDefault();
+  $("#errormsg").text("")
+
+  // This line grabs the input from the textbox
+  var topic = $("#topic-input").val().trim();
+
+  if (topic === "") {
+    console.log("empty");
+    return;
   }
+  /////  verify there is content for this 
+  var queryURL = `https://api.giphy.com/v1/gifs/search?q=${ topic }&api_key=U2E0KeRa320EgNxloxFE3VwHL3OAKsxA&limit=${ limitImg }`;
 
+  // Creating an AJAX call for the specific topic button being clicked
 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
 
-  // This function handles events where a topic button is clicked
-  $("#add-topics").on("click", function (event) {
+    console.log(response);
 
-    event.preventDefault();
+    if (response.data.length > 0) {
 
-    // This line grabs the input from the textbox
-    var topic = $("#topic-input").val().trim();
-    
-    if (topic === "") {
-      console.log("empty");
-      return;
+      // Adding topic from the textbox to our array
+      topics.push(topic);
+      localStorage.clear();
+      localStorage.setItem("movies", topics);
+
+      // Calling renderButtons which handles the processing of our topic array
+      renderButtons();
+
+      // Calling to displayImages direct from the submit new topic
+      displayImages(topic);
+
+      // clear the input text
+      $("#topic-input").val("");
     }
+    else {
+      $("#errormsg").text("no images for this topic")
+    }
+  })
 
-    // Adding topic from the textbox to our array
-    topics.push(topic);
-    localStorage.clear();
-    localStorage.setItem("movies", topics);
+});
 
-    // Calling renderButtons which handles the processing of our topic array
-    renderButtons();
+// Adding a click event listener to all elements with a class of "topic-btn"
+$(document).on("click", ".topic-btn", getTopic);
 
-    // Calling to displayImages direct from the submit new topic
-    displayImages(topic);
+// $(document).on("click", ".image-click", switchImages);
+$(document).on("mouseenter mouseleave", ".image-click", switchImages);
 
-    // clear the input text
-    $("#topic-input").val("");
-   
-  });
+// Getting the value from the localStorage
 
-  // Adding a click event listener to all elements with a class of "topic-btn"
-  $(document).on("click", ".topic-btn", getTopic);
+if (localStorage.getItem("movies") === null) {
 
-  $(document).on("click", ".image-click", switchImages);
+  // Initial array of topics
+  var topicsFirstTime = [ "Matrix the movie", "Alien the movie", "Ex-Machina", "Star Wars", "Terminator", "Blade Runner", "Back to the future", "Mad Max", "2001 the movie", "Star Trek", "Dune the movie" ];
+  localStorage.setItem("movies", topicsFirstTime);
+}
 
-
-  // Getting the value from the localStorage
-  
-  if (localStorage.getItem("movies") === null) {
-    
-    // Initial array of topics
-    var topicsFirstTime = ["Matrix the movie", "Alien the movie", "Ex-Machina", "Star Wars", "Terminator", "Blade Runner", "Back to the future", "Mad Max", "2001 the movie", "Star Trek", "Dune the movie"];
-    localStorage.setItem("movies", topicsFirstTime);
-  }
-
-  topicsSTR = localStorage.getItem("movies");
-  console.log("topicsSTR: " + topicsSTR);
-  topics = topicsSTR.split(",");
-  console.log("topics: " + topics);
+topicsSTR = localStorage.getItem("movies");
+console.log("topicsSTR: " + topicsSTR);
+topics = topicsSTR.split(",");
+console.log("topics: " + topics);
 
 
-  // Calling the renderButtons function to display the intial buttons
-  renderButtons();
-  displayImages(topics[Math.floor(Math.random()*topics.length)]);
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
+displayImages(topics[ Math.floor(Math.random() * topics.length)]);
 
 
